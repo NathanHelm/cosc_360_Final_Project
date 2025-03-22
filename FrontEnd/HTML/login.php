00c4591cb75d82919de9a805db6f880916f7a421
Mridul Nohria <mridulnohria2004@gmail.com>
2025-03-22 13:33:31 -0700

    adds security for hashed password

--- FrontEnd/HTML/login.php
+++ FrontEnd/HTML/login.php
@@ -3,17 +3,21 @@
     
     try{
         $pdo = new PDO(DB_CONNSTR, DB_USERNAME, DB_PASSWORD);
+
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $username = pdo->quote($_POST['username']);
-        $password = pdo->quote($_POST['password']);
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
         $sql = "SELECT * FROM users WHERE username = :username";
         $stmt = $conn->prepare($sql);
         $stmt->bindValue(':username', $username);
         $stmt->bindValue(':password', $password);
         $stmt->execute();
+
         if($res = $stmt->fetch()){
             //start a session here;
-            if($res['password'] == $password){
+            if(password_verify($password, $res['password'])){
                 header("Location: ./home.html");
             } else {
                 echo "Invalid password";
