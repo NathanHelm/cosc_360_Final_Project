4558ae4ad61c828c9cbf19e7976f77b1b96d00a7
Nathan Helm <nathanhelm567@gmail.com>
2025-03-23 13:38:12 -0700

    minor change to user.sql, added email column. signup validation for backend complete (see signup_process.php).

--- Database/user.sql
+++ Database/user.sql
@@ -3,9 +3,15 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 22, 2025 at 10:50 PM
+-- Generation Time: Mar 23, 2025 at 08:26 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
+
+
+/*
+I've updated the sql dump-- now there is a varchar column : email 
+-nathan
+*/
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,9 +39,17 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `user_image` varchar(255) NOT NULL,
   `role` enum('user','admin') NOT NULL DEFAULT 'user',
-  `isActive` tinyint(1) NOT NULL DEFAULT 1
+  `isActive` tinyint(1) NOT NULL DEFAULT 1,
+  `email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `user`
+--
+/*
+INSERT INTO `user` (`user_id`, `username`, `password`, `user_image`, `role`, `isActive`, `email`) VALUES
+(1, 'Jana', 'encryptedpassword', 'img.jpeg', 'user', 1, '');
+*/
 --
 -- Indexes for dumped tables
 --
@@ -55,7 +69,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ FrontEnd/HTML/signup_process.php
@@ -0,0 +1,84 @@
+<?php 
+require_once 'config.php';
+//making a basic connection to database
+try{
+   
+    $pdo = new PDO(DB_CONNSTR, DB_USERNAME, DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->query("use test_db"); //for some reason, this code is nessecary to make proper connection on my end :/ 
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $userImage = $_POST['user_image'];
+    $email = $_POST['email'];
+
+    //check for duplicate password
+    if(!isset($username) || !isset($password) || !isset($email) || !isset($userImage))
+    {
+        echo "reading null values";
+        exit;
+      //  header("location: signup.php"); //some internal error, this should not happen after js validation.
+    }
+    $sql = "select * from user where username = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(1, $username);
+    $stmt->execute();
+    $rows = $stmt->fetchAll();
+    
+    if(IsDuplicateUsername(count($rows)))
+    {
+        //return to signup page, add alert to script. 
+        header("location: signup.php?duplicate=true");
+        exit;
+    }
+
+    //insert values
+    $password = HashPassword($password);
+
+    $sql = "INSERT INTO user (username, password, user_image, role, isActive, email) 
+    VALUES(:username, :password, :userImage, 'user', 1, :email)";
+    
+    /*"INSERT INTO user (username, password, user_image, role, isActive, email) 
+    VALUES($username, $password, $userImage, 'user', 1, $email)";*/
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(":username", $username);
+    $stmt->bindValue(":password", $password);
+    $stmt->bindValue(":userImage", $userImage);
+    $stmt->bindValue(":email", $email);
+    $stmt->execute();
+    
+    //echo "values are inserted to DB!" . "<br> $username, $password, $userImage, 'user', 1, $email";
+    //set session variables
+    session_start();
+
+    $_SESSION['username'] = $username;
+    $_SESSION['email'] = $email;
+    $_SESSION['user_image'] = $userImage;
+
+    header("location: home.html");
+    $pdo = null;
+}
+catch(PDOException $e)
+{
+    $e->getMessage();
+}
+
+function IsDuplicateUsername($rowCount)
+{
+    if($rowCount > 0)
+    {
+        return true;
+    }
+    return false;
+
+}
+function HashPassword($password)
+{
+    $hash = password_hash($password, 
+    PASSWORD_DEFAULT);
+    return $hash;
+}
+
+ 
+?>
