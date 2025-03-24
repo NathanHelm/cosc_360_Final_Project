6623521519ab039ac38a977e43ab5d0861c5b1ba
Mridul Nohria <mridulnohria2004@gmail.com>
2025-03-24 11:41:20 -0700

    new file upload created

--- FrontEnd/HTML/createProduct.php
+++ FrontEnd/HTML/createProduct.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     }
 
-    $targetDir = "../../../uploads/"; 
+    $targetDir = "../uploads/"; 
     $imageName = uniqid() . "_" . basename($image["name"]);
     $targetFilePath = $targetDir . $imageName;
 
