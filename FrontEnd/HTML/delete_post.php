74b4d72da7d95912c947ce59c080c533dde9eb48
Mridul Nohria <mridulnohria2004@gmail.com>
2025-04-09 21:11:04 -0700

    Delete post is correct and user profile function

--- FrontEnd/HTML/delete_post.php
+++ FrontEnd/HTML/delete_post.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
-require_once '';
+require_once '../../config.php';
 
-// 1. Check if user is an admin
+
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
     echo "<script>alert('Access denied. Admins only.'); window.location.href = 'login.php';</script>";
     exit;
 }
 
-//2. Validate post_id
+
 if (!isset($_GET['post_id']) || !is_numeric($_GET['post_id'])) {
     echo "<script>alert('Invalid product ID.'); window.location.href = 'admin_dashboard.php';</script>";
     exit;
@@ -20,11 +20,11 @@ try {
     $pdo = new PDO(DB_CONNSTR, DB_USERNAME, DB_PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // 3. Delete the product
+    
     $stmt = $pdo->prepare("DELETE FROM products WHERE product_id = ?");
     $stmt->execute([$postId]);
 
-    // 4. Redirect back to admin dashboard
+    
     header("Location: admin_dashboard.php?delete=success");
     exit;
 
--- FrontEnd/HTML/profile.php
+++ FrontEnd/HTML/profile.php
@@ -37,6 +37,7 @@ if (!isset($_SESSION['username'])) {
             <button type="submit" id="signOut">Log out</button>
             </form>
             </li>
+            <li><a href="./userProfile.php"><button id="profile">Profile</button></a></li>
             <?php else: ?>
             <li><a href="./login.php"><button id="signIn">Sign in</button></a></li>
             <li><a href="./signup.php"><button id="register">Register</button></a></li>
